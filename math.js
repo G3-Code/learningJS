9b562fb944baa2366b0027ca066589647a053977
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-12 17:24:27 -0500

    equality and math object explored

---
+++ math.js
@@ -0,0 +1,9 @@
+var num = 5.632,
+  num1 = 15,
+  num2 = -10;
+console.log(Math.random());
+console.log(Math.round(num));
+console.log(Math.min(num, num1, num2));
+console.log(Math.max(num, num1, num2));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
